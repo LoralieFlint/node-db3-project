a93fbd49ca8807caa806253bc605db59f51c5dd0
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-01-07 18:50:25 -0700

    only update and delete left

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -10,7 +10,21 @@ function findById(id) {
     .first()
 }
 
+function findSteps(id) {
+    return db("schemes as sc")
+    .join("steps as st", "sc.id", "st.scheme_id")
+    .where({scheme_id: id})
+    .select("st.id", "sc.scheme_name", "st.step_number", "st.instructions")
+}
+
+async function add(scheme) {
+   const [id] = await db("schemes").insert(scheme)
+   return db("schemes").where({ id }).first()
+}
+
 module.exports = {
     find,
     findById,
+    findSteps,
+    add,
 }
