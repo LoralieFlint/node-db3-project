2b63a47e0b1f4c90c6c27b00269803074b125e35
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-01-07 19:21:07 -0700

    one stretch goal reached to add steps

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -21,10 +21,32 @@ async function add(scheme) {
    const [id] = await db("schemes").insert(scheme)
    return db("schemes").where({ id }).first()
 }
+function update(changes, id){
+    return db("schemes").where({ id }).update(changes) .then((res) => {
+        return findById(id)
+    })
+ }
+
+function remove(id) {
+    return db('schemes').where({ id }).del();
+}
 
 module.exports = {
     find,
     findById,
     findSteps,
     add,
+    update,
+    remove,
+    addStep,
 }
+
+function addStep(step, id) {
+    const stepData = { scheme_id: id, ...step }
+    
+    return db('steps')
+    .insert(stepData)
+    .then(id => ({
+      ...stepData, id: id[0]
+    }))
+  }
